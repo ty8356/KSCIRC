a95ea5b6b1a30b040c7a7b08151a56d47ea7cb4d
Tyler Stephenson <ty8356@gmail.com>
2022-01-13 18:22:49 -0500

    fix naming

---
+++ Services/GeneService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KSCIRC.Interfaces.Services;
+using KSCIRC.Models.Model;
+
+namespace KSCIRC.Services
+{
+    public class GeneService : IGeneService
+    {
+        private readonly KSCIRC_devContext _context;
+
+        public GeneService(
+            KSCIRC_devContext context
+        )
+        {
+            _context = context;
+        }
+    }
+}
